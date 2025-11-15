dcee23a614746a840d5d961ad4b7485d69e6852f
artdarek <artdarek@gmail.com>
2025-11-15 16:53:08 +0100

    Version bump to 1.4.0

--- src/merchant_tycoon/__init__.py
+++ src/merchant_tycoon/__init__.py
@@ -4,7 +4,7 @@ Merchant Tycoon - A terminal-based trading game
 Buy low, sell high, travel between cities, and become a merchant tycoon!
 """
 
-__version__ = "0.9.1"
+__version__ = "1.4"
 __author__ = "Dariusz Przada"
 
 __all__ = []
